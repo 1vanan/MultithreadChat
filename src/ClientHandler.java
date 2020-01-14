49b0bc91f1f64ffa135c013db604878ce9d6cb77
Fedotov <vanen31@gmail.com>
2020-01-14 17:03:46 +0100

    initial commit

---
+++ src/ClientHandler.java
@@ -0,0 +1,12 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    public ClientHandler (Socket s, String s1, DataInputStream dis, DataOutputStream dos) {
+    }
+
+    @Override public void run() {
+
+    }
+}
